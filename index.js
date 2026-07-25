e477586d714bf9e8c3517092c5ed9e72f239ed0e
Abhinav Kumar <abhinav291202@gmail.com>
2026-07-26 06:03:18 +1000

    plaintext relay

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get('/', (req,res) => {
 });
 
 io.on('connection', (socket)=>{
-  console.log('user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 });
 
 server.listen(3000, () =>{
